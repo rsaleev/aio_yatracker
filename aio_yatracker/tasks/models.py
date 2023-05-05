a858b5d166f27a74f91fb6191897d2975449c8be
Rostislav Aleev <rs.aleev@gmail.com>
2023-05-05 14:35:26 +0300

    + issue mehods models

---
+++ aio_yatracker/tasks/models.py
@@ -0,0 +1,9 @@
+import typing
+from datetime import datetime
+from..common.issue import IssueModel
+from ..base import TrackerModel
+
+
+
+class IssueParametersResponse(TrackerModel):
+    __root__ = typing.List[IssueModel]
