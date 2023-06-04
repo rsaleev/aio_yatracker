9c5abdbe715103e39faaff2a9a5cbb24fea0b6b7
Rostislav Aleev <rs.aleev@gmail.com>
2023-06-04 21:45:08 +0300

    + macros

--- aio_yatracker/macros/models.py
+++ aio_yatracker/macros/models.py
@@ -1,13 +1,30 @@
-from ..base import BaseClient, TrackerModel
-from ..common import Macros
+from typing import List
+
+from ..base import TrackerModel
+from ..common import Macros, MacrosFieldChanges
 
 
+
+class MacrosRequest(TrackerModel):
+    name:str 
+    body:str 
+    field_changes:List[MacrosFieldChanges]
 class MacrosResponse(Macros):
     pass
 
+class MacrosCreateRequest(MacrosRequest):
+    pass
 
+class MacrosCreateResponse(Macros):
+    pass 
 
-__all__ = ["MacrosResponse"]
+class MacrosEditRequest(MacrosRequest):
+    pass
+
+class MacrosEditResponse(Macros):
+    pass
+
+__all__ = ["MacrosResponse", "MacrosCreateRequest", "MacrosCreateResponse", "MacrosEditRequest", "MacrosEditResponse"]
 
 
 
--- aio_yatracker/macros/query.py
+++ aio_yatracker/macros/query.py
@@ -1,7 +1,7 @@
 from typing import AsyncGenerator
 
 from ..base import BaseClient, RequestParams
-from .models import MacrosResponse
+from .models import *
 
 ENDPOINT = "queues"
 
@@ -51,3 +51,64 @@ async def get(client: BaseClient, queue_id: str, macros_id: str) -> MacrosRespon
     response.close()
     return MacrosResponse.parse_obj(response_data)
 
+
+async def create(
+    client: BaseClient, queue_id: str, data: MacrosCreateRequest
+) -> MacrosCreateResponse:
+    """
+    create macros
+
+    :param client: client instance
+    :type client: BaseClient
+    :param queue_id: e.g. 'QUEUE-1'
+    :type queue_id: str
+    :param data: request body
+    :type data: MacrosCreateRequest
+    :return: response data
+    :rtype: MacrosCreateResponse
+    """
+    url = f"{ENDPOINT}/{queue_id}/macros"
+    response = await client.post(url, data)
+    response_data = await response.json()
+    response.close()
+    return MacrosCreateResponse.parse_obj(response_data)
+
+
+async def edit(
+    client: BaseClient, queue_id: str, macros_id: int, data: MacrosEditRequest
+) -> MacrosEditResponse:
+    """
+    edit macros
+
+    :param client: client instance
+    :type client: BaseClient
+    :param queue_id: e.g. QUEUE-1
+    :type queue_id: str
+    :param macros_id: e.g. 3
+    :type macros_id: int
+    :param data: request body
+    :type data: MacrosEditRequest
+    :return: request data
+    :rtype: MacrosEditResponse
+    """
+    url = f"{ENDPOINT}/{queue_id}/macros/{macros_id}"
+    response = await client.patch(url, data)
+    response_data = await response.json()
+    response.close()
+    return MacrosEditResponse.parse_obj(response_data)
+
+
+async def delete(client: BaseClient, queue_id: str, macros_id: int):
+    """
+    delete macros
+
+    :param client: client instance
+    :type client: BaseClient
+    :param queue_id: e.g. QUEUE-1
+    :type queue_id: str
+    :param macros_id: e.g. 9
+    :type macros_id: int
+    """
+    url = f"{ENDPOINT}/{queue_id}/macros/{macros_id}"
+    response = await client.delete(url)
+    response.close()
